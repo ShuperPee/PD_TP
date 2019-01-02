6ac2976c593ece601c513f81f2bb4d35736f1224
Carlos Balsas <carlos.balsas17@gmail.com>
2019-01-02 23:35:33 +0000

    02/01 V6 TextUI

--- PD_TP/src/TextUI.java
+++ PD_TP/src/TextUI.java
@@ -20,6 +20,7 @@ public class TextUI implements Observer{
         String [] words;
         int num;
         String username, password;
+        Scanner ler = new Scanner(System.in);
         
         System.out.println("\n=== BEM-VINDO! ===\n");
   
@@ -53,17 +54,40 @@ public class TextUI implements Observer{
                     return;
 
                 case '1':
-                    
+                    boolean sucess = false;
                     System.out.println("Username: ");
-                    Scanner ler = new Scanner(System.in);
                     username = ler.nextLine();
                     System.out.println("Password: ");
-                    Scanner ler2 = new Scanner(System.in);
-                    password = ler2.nextLine();
+                    password = ler.nextLine();
                     
+                    sucess = client.register(username, password);
+                    if(sucess == true){
+                        System.out.println("Registo efetuado com sucesso!");
+                    }
+                    else{
+                        System.out.println("Registo falhou!");
+                    }
+                    
+
                     return;
                 
                 case '2':
+                    
+                    System.out.println("Username: ");
+                    username = ler.nextLine();
+                    System.out.println("Password: ");
+                    password = ler.nextLine();
+                    
+                    
+                   sucess = client.doLogin(username, password);
+                    
+                    if(sucess == true){
+                        System.out.println("Registo efetuado com sucesso!");
+                    }
+                    else{
+                        System.out.println("Registo falhou!");
+                    }
+                    
                     return;
                 
                     
@@ -75,7 +99,72 @@ public class TextUI implements Observer{
             
         } 
     
-    } 
+    }
+    
+    public void uiLogin()
+    {
+        Scanner sc = new Scanner(System.in);
+        BufferedReader bin = new BufferedReader(new InputStreamReader(System.in));
+        String option, fileName;
+        char c;
+        String [] words;
+        int num;
+        String username, password;
+        Scanner ler = new Scanner(System.in);
+        
+        System.out.println("\n=== BEM-VINDO! ===\n");
+  
+        
+        
+        while (true){
+            
+            do{
+                
+                System.out.println();
+                System.out.println("0 - Quit");
+                System.out.println();
+                System.out.println("1 - Send Message");
+                System.out.println("2 - Request File");
+                System.out.println();
+                System.out.print("> ");
+
+                option = sc.next();
+                
+                if(option.length() >= 1)
+                    c = option.charAt(0);
+                else
+                    c = ' ';
+                
+            }while(c < '0' || c > '2');
+            
+            switch(c){
+                
+                case '0':
+                    quit = true;
+                    return;
+
+                case '1':
+                    boolean sucess = false;
+                    System.out.println("Message: ");
+                    String msg = ler.nextLine();
+                    
+                    client.sendMsg(msg);
+                    
+                    return;
+                
+                case '2':
+                    
+                   System.out.println("File Name: ");
+                   fileName = ler.nextLine();
+                   client.requestFile(fileName, client.getlocalDirectory, client.getPort, client.getAddr);
+                default:
+                    return;
+                    
+            } 
+            
+        } 
+    
+    }
 
     @Override
     public void update(Observable o, Object arg) {
